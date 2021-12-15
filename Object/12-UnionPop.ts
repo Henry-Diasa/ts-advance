c5900e7f5295bd8d9bc381f6c543f648ae269ecf
fuqiang <823342479@qq.com>
2021-12-15 10:55:53 +0800

    UnionPop

--- Object/12-UnionPop.ts
+++ Object/12-UnionPop.ts
@@ -1,6 +1,18 @@
-export {} 
-type UnionPop<T> = ''
+export {};
+import { IsAny } from "./8-IsAny";
+export type UnionPop<U> = IsAny<U> extends true
+  ? any
+  : boolean extends U
+  ? boolean
+  : (
+      (U extends U ? (fn: (u: U) => any) => any : never) extends (
+        u: infer f1
+      ) => any
+        ? f1
+        : never
+    ) extends (f2: infer F2) => any
+  ? F2
+  : never;
 
 type a = 1 | 2 | 3;
 type b = UnionPop<a>; // 3
-
--- Object/13-UnionToTuple.ts
+++ Object/13-UnionToTuple.ts
@@ -1,7 +1,18 @@
+import { UnionPop } from "./12-UnionPop";
+
+type UnionToTuple<T, TT = T, R extends any[] = []> = [T] extends [R[number]]
+  ? R
+  : UnionToTuple<T, Exclude<TT, UnionPop<TT>>, [UnionPop<TT>, ...R]>;
+
 type a = UnionToTuple<1 | 2 | 3>; // [1,2,3]
 type b = UnionToTuple<1 | string | boolean>; // [1,string,boolean]
 type c = UnionToTuple<any>; // [any]
 
 type Q1 = UnionToTuple<string | number | symbol>; // [symbol,number,string]
 type Q2 = UnionToTuple<string | number | symbol | boolean>; // [boolean,symbol,number,string]
+
 type Q3 = UnionToTuple<string | number | symbol | boolean | [boolean]>; // [boolean,[boolean],symbol,number,string]
+
+// type a1 = 1 | 2 | 3;
+// type a2 = [1, 2, 3];
+// type aa = [a1] extends [a2[number]] ? true : false;
